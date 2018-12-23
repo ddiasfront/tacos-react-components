10bf7050ffadc70c8939f70732275e2e888798c5
Dias <d.dias@accenture.com>
2018-12-22 22:22:13 -0200

    adding support to next link

--- stories/index.js
+++ stories/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { Button } from '@storybook/react/demo';
+import { LinkComponent } from '../lib/wrapper';
+import { action } from '@storybook/addon-actions';
+import { linkTo } from '@storybook/addon-links';
+import { withKnobs, text, boolean, number } from '@storybook/addon-knobs';
+import { specs, describe, it } from 'storybook-addon-specifications';
 
-storiesOf('Button', module)
+const storiesLink = storiesOf('Tacos Components', module);
+
+storiesLink
   .add('with text', () => (
     <Button>Hello Button</Button>
   ))
   .add('with some emoji', () => (
     <Button><span role="img" aria-label="so cool">😀 😎 👍 💯</span></Button>
   ));   
+
+  storiesLink
+    .add('Pure text inside', () => (
+      <LinkComponent>
+        Text
+      </LinkComponent>
+    ));
